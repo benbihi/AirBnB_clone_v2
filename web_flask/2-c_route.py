bbe98448d7183b2ba26d1581507ea9f7762db62e
benbihi <elm.benbihi@gmail.com>
2024-07-31 17:19:43 -0700

    task 2 web framework

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" 0. Script to start a Flask web application """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return “HBNB”"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def cistext():
+    """ Returns some text. """
+    text_trimmed = text.replace('_', ' ')
+    return 'c {}'.format(text_trimmed)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
